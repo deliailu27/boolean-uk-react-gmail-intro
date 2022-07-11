8a59b9fca455d3ca25e90457ca16c1d7580bfa9b
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-07-11 17:18:58 +0100

    standard requirement

---
+++ src/component/MainComponent/Main.js
@@ -0,0 +1,11 @@
+import NavEmailToolBar from "./NavEmailToolBar.js";
+import Article from "./Article.js";
+
+export default function Main() {
+  return (
+    <main className="email-view">
+      <NavEmailToolBar />
+      <Article />
+    </main>
+  );
+}
